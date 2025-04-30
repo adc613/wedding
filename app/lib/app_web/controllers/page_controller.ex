f6a29a0627c2fcec2f4836d46b9e4d07e3a0668d
Adam Collins <adc613@gmail.com>
2025-04-29 21:41:20 -0500

    Initialize dockerfile

--- app/lib/app_web/controllers/page_controller.ex
+++ app/lib/app_web/controllers/page_controller.ex
@@ -2,8 +2,30 @@ defmodule AppWeb.PageController do
   use AppWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
     render(conn, :home)
+  end
+
+  def story(conn, _params) do
+    render(conn, :story)
+  end
+
+  def photos(conn, _params) do
+    render(conn, :photos)
+  end
+
+  def registry(conn, _params) do
+    render(conn, :registry)
+  end
+
+  def rsvp(conn, _params) do
+    render(conn, :rsvp)
+  end
+
+  def travel(conn, _params) do
+    render(conn, :travel)
+  end
+
+  def things_to_do(conn, _params) do
+    render(conn, :things_to_do)
   end
 end
