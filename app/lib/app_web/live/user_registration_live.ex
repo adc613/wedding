14c4c196a87b2156d6e2da64d0df6edf5b07ff07
Adam Collins <adc613@gmail.com>
2025-05-28 17:49:08 -0500

    Delete code paths for creating an account

--- app/lib/app_web/live/user_registration_live.ex
+++ app/lib/app_web/live/user_registration_live.ex
@@ -17,27 +17,6 @@ defmodule AppWeb.UserRegistrationLive do
           to your account now.
         </:subtitle>
       </.header>
-
-      <.simple_form
-        for={@form}
-        id="registration_form"
-        phx-submit="save"
-        phx-change="validate"
-        phx-trigger-action={@trigger_submit}
-        action={~p"/users/log_in?_action=registered"}
-        method="post"
-      >
-        <.error :if={@check_errors}>
-          Oops, something went wrong! Please check the errors below.
-        </.error>
-
-        <.input field={@form[:email]} type="email" label="Email" required />
-        <.input field={@form[:password]} type="password" label="Password" required />
-
-        <:actions>
-          <.button phx-disable-with="Creating account..." class="w-full">Create an account</.button>
-        </:actions>
-      </.simple_form>
     </div>
     """
   end
@@ -51,23 +30,6 @@ defmodule AppWeb.UserRegistrationLive do
       |> assign_form(changeset)
 
     {:ok, socket, temporary_assigns: [form: nil]}
-  end
-
-  def handle_event("save", %{"user" => user_params}, socket) do
-    case Accounts.register_user(user_params) do
-      {:ok, user} ->
-        {:ok, _} =
-          Accounts.deliver_user_confirmation_instructions(
-            user,
-            &url(~p"/users/confirm/#{&1}")
-          )
-
-        changeset = Accounts.change_user_registration(user)
-        {:noreply, socket |> assign(trigger_submit: true) |> assign_form(changeset)}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, socket |> assign(check_errors: true) |> assign_form(changeset)}
-    end
   end
 
   def handle_event("validate", %{"user" => user_params}, socket) do
